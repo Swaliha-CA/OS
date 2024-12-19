b98debc4d73e7538cf62b542cfaca1496b27a197
Swaliha-CA <swalihaswali100@gmail.com>
2024-12-19 12:20:07 +0530

    Add files via upload

---
+++ palindrome.sh
@@ -0,0 +1,17 @@
+echo "enter the number"
+read a
+num=$a
+rev=0
+while [ $a -gt 0 ]
+do
+ digits=$(( $a % 10 ))
+ rev=$((rev * 10 + $digits ))
+ a=$(( $a / 10 ))
+done
+if [ $rev -eq $num ] 
+then
+  echo " it is palindrome "
+else
+   echo " it is not palindrome"
+fi
+
