d6d5a1bf33bdc70e48baf3a7223b1767e4f264f7
Swaliha-CA <swalihaswali100@gmail.com>
2025-02-10 11:53:17 +0530

    Add files via upload

---
+++ EXP6/fcfs.c
@@ -0,0 +1,54 @@
+#include<stdio.h>  
+
+void main() {     
+    int p[10], wt[10], tt[10], n, bt[10];     
+    float avg_tt = 0.0, avg_wt = 0.0;      
+
+    printf("Enter the number of processes: ");     
+    scanf("%d", &n);            
+
+    printf("Enter the burst time of each process:\n");     
+    for (int i = 0; i < n; i++) {         
+        p[i] = i + 1;          
+        printf("p%d: ", i + 1);         
+        scanf("%d", &bt[i]);     
+    }      
+
+    wt[0] = 0;      
+
+    for (int i = 1; i < n; i++) {         
+        wt[i] = wt[i - 1] + bt[i - 1];          
+        avg_wt += wt[i];     
+    }      
+
+    avg_wt /= n;       
+
+    for (int i = 0; i < n; i++) {         
+        tt[i] = wt[i] + bt[i];          
+        avg_tt += tt[i];     
+    }      
+
+    avg_tt /= n;       
+
+    printf("\nProcess\tBurst Time\tWaiting Time\tTurnaround Time\n");     
+    for (int i = 0; i < n; i++) {         
+        printf("p%d\t\t%d\t\t%d\t\t%d\n", p[i], bt[i], wt[i], tt[i]);     
+    }          
+
+    printf("\nAverage waiting time: %.2f\n", avg_wt);     
+    printf("Average turnaround time: %.2f\n", avg_tt);      
+
+    printf("\nGantt chart\n\n");     
+    printf("\n----------------------\n");         
+
+    for(int i = 0; i < n; i++) {
+        printf("\t p%d\t||", p[i]);
+    }
+
+    printf("\n----------------------\n");
+
+   
+    
+    
+}
+
