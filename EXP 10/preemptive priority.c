f84f982aa8afcd8b8e6b33be17f12d7b33c4bb6d
Swaliha <swalihaswali100@gmail.com>
2025-03-19 19:42:02 +0530

    Update preemptive priority.c

--- EXP 10/preemptive priority.c
+++ EXP 10/preemptive priority.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 void main() {
-    int n, at[10], bt[10], pr[10], rt[10], ct[10], tt[10], wt[10];
-    int complete = 0, time = 0, highest = -1, min_pr;
+    int n, at[10], bt[10], rt[10], ct[10], tt[10], wt[10];
+    int complete = 0, time = 0, shortest = -1, min_rt;
     float avg_wt = 0, avg_tt = 0;
 
     printf("Enter the number of processes: ");
@@ -21,53 +21,47 @@ void main() {
         rt[i] = bt[i];
     }
 
-    printf("Enter priorities (lower number = higher priority):\n");
-    for (int i = 0; i < n; i++) {
-        printf("P%d: ", i + 1);
-        scanf("%d", &pr[i]);
-    }
-
     printf("\nGantt chart\n");
     printf("-------------------------------------------------\n");
 
     while (complete < n) {
-        highest = -1;
-        min_pr = 9999;
+        shortest = -1;
+        min_rt = 9999;
 
         for (int i = 0; i < n; i++) {
-            if (at[i] <= time && rt[i] > 0 && pr[i] < min_pr) {
-                highest = i;
-                min_pr = pr[i];
+            if (at[i] <= time && rt[i] > 0 && rt[i] < min_rt) {
+                shortest = i;
+                min_rt = rt[i];
             }
         }
 
-        if (highest == -1) {
+        if (shortest == -1) {
             time++;
             continue;
         }
 
-        printf("  P%d  |", highest + 1);
-        rt[highest]--;
+        printf("  P%d  |", shortest + 1);
+        rt[shortest]--;
         time++;
 
-        if (rt[highest] == 0) {
+        if (rt[shortest] == 0) {
             complete++;
-            ct[highest] = time;
+            ct[shortest] = time;
         }
     }
 
     printf("\n-------------------------------------------------\n");
 
     for (int i = 0; i < n; i++) {
-        wt[i] = ct[i] - at[i];          
-        tt[i] = wt[i] - bt[i];          
+        tt[i] = ct[i] - at[i];          // Turnaround Time
+        wt[i] = tt[i] - bt[i];          // Waiting Time
         avg_wt += wt[i];
         avg_tt += tt[i];
     }
 
-    printf("\nProcess\tAT\tBT\tPr\tCT\tTT\tWT\n");
+    printf("\nProcess\tAT\tBT\tCT\tTT\tWT\n");
     for (int i = 0; i < n; i++) {
-        printf("P%d\t%d\t%d\t%d\t%d\t%d\t%d\n", i + 1, at[i], bt[i], pr[i], ct[i], tt[i], wt[i]);
+        printf("P%d\t%d\t%d\t%d\t%d\t%d\n", i + 1, at[i], bt[i], ct[i], tt[i], wt[i]);
     }
 
     printf("\nAverage Turnaround Time: %.2f", avg_tt / n);
